f6a62dda7a43b12d71adb8a173ff604c09d555a7
sunjudev0213 <sunshineju0213@gmail.com>
2023-05-02 00:14:23 +0900

    remove unnecessary files

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-const tools = require('./lib/tools');
 const log = require('./lib/logger')();
 
 const middlewares = require('./middlewares');
